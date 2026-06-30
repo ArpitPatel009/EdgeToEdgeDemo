c803d91c83a7605207c14424fc608b8eff1b6638
Arpit Patel <108341293+ArpitPatel009@users.noreply.github.com>
2026-06-29 22:53:50 -0400

    Added inset for activity

--- app/src/main/java/com/example/edgetoedgedemo/LoginActivity.kt
+++ app/src/main/java/com/example/edgetoedgedemo/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.edgetoedgedemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import com.example.edgetoedgedemo.sdk.SecureLoginFragment
 
@@ -15,11 +17,11 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(window)
         setContentView(R.layout.activity_login)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginScrollView)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         supportFragmentManager.commit {
             replace(R.id.loginFragment, SecureLoginFragment(), CIAM_FRAGMENT_TAG)
